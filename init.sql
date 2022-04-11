72d05c55df84c7adbc2daca4e3fc5e144097a084
kasyapakuchi <kasyapa.kvsm@gmail.com>
2022-04-11 21:58:06 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
+//schema
 create schema vaccinevendor;
 
+//DDL
 CREATE TABLE `vendor` (
   `vendor_id` varchar(256) NOT NULL,
   `vendor_name` varchar(256) DEFAULT NULL,
@@ -19,4 +21,31 @@ CREATE TABLE `vaccines` (
   KEY `vendor_id` (`vendor_id`),
   CONSTRAINT `vaccines_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendor` (`vendor_id`)
 ) ENGINE=InnoDB;
+
+//DML
+
+INSERT INTO `vaccinevendor`.`vaccines`
+(`vaccine_id`,
+`vendor_id`,
+`vaccine_name`,
+`available_count`,
+`exp_date`)
+VALUES
+("Covid4",
+"V126",
+"CovidVaccine",
+1000,
+now()+100);
+
+
+INSERT INTO `vaccinevendor`.`vendor`
+(`vendor_id`,
+`vendor_name`,
+`location`,
+`city`)
+VALUES
+("V126",
+"Reddys",
+"Telangana",
+"Hyderabad");
 
